cf104e3e50586ae5897f398cdfb7c9a7896fb3d8
hotjupitersinc <190882646+hotjupitersinc@users.noreply.github.com>
2024-12-07 11:01:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const textImage = 'on.png';   // Replace with the path to your text image
 const imageContainer = document.getElementById('image-container');
 
 // Timing variables (in milliseconds)
-const subliminalDuration = 0.1;      // Duration to display the text image (e.g., 10 ms)
-const subliminalInterval = 300;    // Interval between displays (e.g., every 3 seconds)
+const subliminalDuration = 1;      // Duration to display the text image (e.g., 10 ms)
+const subliminalInterval = 3000;    // Interval between displays (e.g., every 3 seconds)
 
 // Function to display the subliminal message
 function showSubliminalMessage() {
